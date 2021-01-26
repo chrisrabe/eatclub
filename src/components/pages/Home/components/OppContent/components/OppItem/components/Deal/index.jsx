20884e2ee98fb24b2b37cf37088fbef5aa3d6680
Chris Rabe <chris.rabe14@gmail.com>
2021-01-26 15:18:19 +1000

    increment and decrement deal

--- src/components/pages/Home/components/OppContent/components/OppItem/components/Deal/index.jsx
+++ src/components/pages/Home/components/OppContent/components/OppItem/components/Deal/index.jsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import colors from 'utils/colors';
 import { KeyboardArrowDown, KeyboardArrowUp } from '@material-ui/icons';
 import Info from '../Info';
 import { Container, ButtonContainer, StyledButton } from './styles';
 
-const Deal = ({ value }) => (
-  <Container>
-    <Info
-      headingText="Deal"
-      valueText={value}
-      valueStyle={{ fontSize: 35, fontWeight: 800, color: colors.primary }}
-    />
-    <ButtonContainer>
-      <StyledButton variant="contained">
-        <KeyboardArrowUp />
-      </StyledButton>
-      <StyledButton variant="contained">
-        <KeyboardArrowDown />
-      </StyledButton>
-    </ButtonContainer>
-  </Container>
-);
+const Deal = ({ value }) => {
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    setNum(parseInt(value, 10));
+  }, [value]);
+
+  const increaseDeal = useCallback(() => {
+    if (num < 50) {
+      setNum(num + 5);
+    }
+  }, [setNum, num]);
+
+  const decreaseDeal = useCallback(() => {
+    if (num > 10) {
+      setNum(num - 5);
+    }
+  }, [setNum, num]);
+
+  return (
+    <Container>
+      <Info
+        headingText="Deal"
+        valueText={`${num}%`}
+        valueStyle={{ fontSize: 35, fontWeight: 800, color: colors.primary }}
+      />
+      <ButtonContainer>
+        <StyledButton variant="contained" onClick={increaseDeal}>
+          <KeyboardArrowUp />
+        </StyledButton>
+        <StyledButton variant="contained" onClick={decreaseDeal}>
+          <KeyboardArrowDown />
+        </StyledButton>
+      </ButtonContainer>
+    </Container>
+  );
+};
 
 Deal.propTypes = {
   value: PropTypes.string.isRequired,
